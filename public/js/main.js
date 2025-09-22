70b281cd32838eaa667b1ae6599dbabb3d8fb21c
Isabel Cruz Rivera <iscruzrivera@gmail.com>
2025-09-21 22:14:17 -0400

    initial structure

---
+++ public/js/main.js
@@ -0,0 +1,131 @@
+// FRONT-END (CLIENT) JAVASCRIPT HERE
+const results = async function (event) {
+  const username = localStorage.getItem("username")
+  const response = await fetch(`/results?username=${encodeURIComponent(username)}`)
+  const data = await response.json()
+
+  const body = document.getElementById("results").getElementsByTagName("tbody")[0]
+  body.innerHTML = ""
+
+  data.forEach((obj, i) => {
+    const row = body.insertRow()
+
+    row.insertCell(0).innerText = obj.task
+    row.insertCell(1).innerText = obj.priority
+    row.insertCell(2).innerText = obj.creation
+    row.insertCell(3).innerText = obj.deadline
+
+    const del = row.insertCell(4)
+    const delButton = document.createElement("button")
+    delButton.innerText = "Delete"
+    const info = JSON.stringify({ id: obj._id, username })
+
+    delButton.onclick = async () => {
+      await fetch("/delete", {
+        method: "POST",
+        body: info,
+        headers: { 'Content-Type': 'application/json' }
+      })
+      results()
+    }
+
+    del.appendChild(delButton)
+
+    const modify = row.insertCell(5)
+    const modifyButton = document.createElement("button")
+    modifyButton.innerText = "Modify"
+
+    modifyButton.onclick = () => openModal(obj, i)
+    modify.appendChild(modifyButton)
+  });
+}
+
+const login = async function (event) {
+  event.preventDefault()
+  const username = document.querySelector("#username").value
+  const password = document.querySelector("#password").value
+
+  const response = await fetch("/login", {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+
+  if (!response.ok) {
+    alert("Invalid username or password");
+    return;
+  }
+  
+  localStorage.setItem("username", username)
+
+  document.getElementById("login").style.display = "none"
+  document.getElementById("app").style.display = "block"
+  results()
+}
+const submit = async function (event) {
+  // stop form submission from trying to load
+  // a new .html page for displaying results...
+  // this was the original browser behavior and still
+  // remains to this day
+  event.preventDefault()
+
+  const username = localStorage.getItem("username")
+  const task = document.querySelector("#task").value
+  const priority = document.querySelector("#priority").value
+  const body = JSON.stringify({ username, task, priority })
+
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  document.querySelector("#task").value = ""
+  results()
+}
+
+const openModal = (obj, i) => {
+  const modal = document.getElementById("modify")
+  document.getElementById("editTask").value = obj.task
+  document.getElementById("editPriority").value = obj.priority
+
+  const save = document.getElementById("save")
+  const close = document.querySelector(".close")
+  const username = localStorage.getItem("username");
+
+  modal.style.display = "flex"
+
+  save.onclick = async () => {
+    const newTask = document.getElementById("editTask").value
+    const newPriority = document.getElementById("editPriority").value
+
+    await fetch("/modify", {
+      method: "POST",
+      body: JSON.stringify({ id: obj._id, username, newTask, newPriority }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    modal.style.display = "none"
+    results()
+  }
+
+  close.onclick = () => {
+    modal.style.display = "none"
+  }
+}
+window.onload = function () {
+  document.getElementById("login-button").onclick = login
+  document.getElementById("task-button").onclick = submit
+
+  if (localStorage.getItem("username")) {
+    document.getElementById("login").style.display = "none"
+    document.getElementById("app").style.display = "block"
+    results()
+  }
+
+  this.document.getElementById("logout-button").onclick = () => {
+    this.localStorage.removeItem("username")
+    this.location.reload()
+  }
+}
---
+++ server.improved.js
@@ -0,0 +1,128 @@
+const express = require('express'),
+  fs = require('fs'),
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000,
+  app = express(),
+  path = require('path')
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const uri = 'mongodb+srv://iscruzrivera_db_user:Athena1068@webware.ifk3fxl.mongodb.net/?retryWrites=true&w=majority&appName=webware'
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let tasksCollection;
+
+async function initDB() {
+  try {
+    await client.connect()
+    const db = client.db('todo')
+    tasksCollection = db.collection('tasks')
+    console.log("Successfully connected.")
+  } catch {
+    console.log("DB connection error: ", err)
+  }
+}
+initDB()
+
+app.use(express.json())
+app.use(express.static(dir))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public.index.html'))
+})
+
+app.get('/results', async (req, res) => {
+  const username = req.query.username
+  const tasks = await tasksCollection.find({username}).toArray()
+  res.json(tasks)
+})
+
+app.post('/submit', async (req, res) => {
+  const { username, task, priority } = req.body
+
+  const creation = new Date().toLocaleDateString()
+  let deadline = new Date(creation)
+
+  if (priority === "high") {
+    deadline.setDate(deadline.getDate() + 1)
+  } else if (priority === "medium") {
+    deadline.setDate(deadline.getDate() + 2)
+  } else if (priority === "low") {
+    deadline.setDate(deadline.getDate() + 3)
+  }
+
+  deadline = deadline.toLocaleDateString()
+
+  const newTask = {
+    username,
+    task,
+    priority,
+    creation,
+    deadline
+  }
+
+  await tasksCollection.insertOne(newTask)
+
+  res.status(200).send('added')
+})
+
+app.post('/delete', async (req, res) => {
+  const { id, username } = req.body
+  await tasksCollection.deleteOne({_id: new ObjectId(id), username})
+
+  res.status(200).send('deleted')
+})
+
+app.post('/modify', async (req, res) => {
+  const { id, username, newTask, newPriority } = req.body
+  const toUpdate = await tasksCollection.findOne({ _id: new ObjectId(id), username })
+
+  if (!toUpdate) {
+    return res.status(400).send('Task does not exist')
+  }
+
+  toUpdate.task = newTask
+  toUpdate.priority = newPriority
+
+  let creation = new Date(toUpdate.creation)
+  let deadline = new Date(creation)
+
+  if (newPriority === "high") {
+    deadline.setDate(deadline.getDate() + 1)
+  } else if (newPriority === "medium") {
+    deadline.setDate(deadline.getDate() + 2)
+  } else if (newPriority === "low") {
+    deadline.setDate(deadline.getDate() + 3)
+  }
+
+  await tasksCollection.updateOne(
+    { _id: new ObjectId(id), username },
+    { $set: { task: newTask, priority: newPriority, deadline: deadline.toLocaleDateString() } }
+  )
+
+  res.status(200).send('modified')
+})
+
+app.post('/login', async(req, res) => {
+  const {username, password} = req.body
+
+  const user = await client.db('todo').collection('users').findOne({username})
+  if (!user || user.password !== password) {
+    return res.status(400).send('Invalid credentials.')
+  }
+
+  await client.db('todo').collection('sessions').insertOne({
+    userId: user._id,
+    created: new Date()
+  })
+
+  return res.status(200).send('Successful login')
+})
+app.listen(process.env.PORT || port)
